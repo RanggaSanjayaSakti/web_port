89c44f733ca209b47eba8833158a2b7d06b1e532
rangga <saktirangga02@gmail.com>
2024-03-24 19:41:32 +0700

    minpro2

---
+++ script.js
@@ -0,0 +1,8 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+});
+
+
---
+++ typed.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('.hero-left h2', {
+        strings: ["Web-Development", "Jasa-Joki", "Trader"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+
+    console.log("Typed.js initialized.");
+
+});
